053e4eba39068fa0bff2eb2b1c62fa94be6c82fe
KinshinyerR <kinshinyer02@gmail.com>
2021-04-26 22:12:12 -0500

    update product Model

--- models/product.model.js
+++ models/product.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  imagen: { type: String },
+  image: { type: String },
   description: { type: String },
 });
 
