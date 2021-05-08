3c2415e761cc4244e969510562dd93645776355a
KinshinyerR <kinshinyer02@gmail.com>
2021-05-08 13:34:32 -0500

    Update ordersController

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const order = require("../models/order.model");
+const { body } = require("express-validator");
+
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
+const Product = require("../models/product.model");
+const validate = require("../middleware/validate");
+const auth = require("../middleware/auth");
+const verifyRole = require("../middleware/role");
 
 const router = express.Router();
 
@@ -10,17 +17,46 @@ router.put("/update/:id", update);
 router.delete("/delete/:id", remove);
 
 function getAll(req, res) {
-  order.find()
-  .then((orders) => res.send(orders))
-  .catch((error) => res.status(400).json(error));
+  Order.find().populate("userId")
+    .then((orders) => res.send(orders))
+    .catch((error) => res.status(400).json(error));
   // res.send("Todos los Ordenes");
 }
 function getById(req, res) {
   console.log(req.params.id);
   res.send(`Ordenes: ${req.params.id}`);
 }
-function create(req, res) {
-  res.send(`Ordenes creado`);
+async function create(req, res) {
+  const order = new Order(req.body);
+  console.log(req.body);
+  const { products } = req.body;
+
+  try {
+    const userDB = await User.findById(req.body.userId);
+    if (!userDB) {
+      res.status(400).send(`Usuario no encontrado`);
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < products.length; i++) {
+      const productDB = await Product.findById(products[i].productId);
+      if (!productDB) {
+        throw new Error(`Producto ID no encontrado ${products[i].productId}`);
+      }
+      products[i].productPrice = productDB.price;
+      products[i].productName = productDB.name;
+      total += products[i].productPrice * products[i].quantity;
+    }
+
+    order.products = products;
+    order.paymentValue = total;
+
+    await order.save();
+    res.json(order);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 }
 function update(req, res) {
   console.log(req.params.id);
--- models/order.model.js
+++ models/order.model.js
@@ -4,10 +4,21 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
   date: { type: Date, required: true },
   paymentType: { type: String, required: true },
-  paymentValue: { type: Number, required: true },
-  status: { type: String, required: true },
-  user: {type: Object, require: true},
-  products: {type: Object, require: true}
+  paymentValue: Number,
+  status: {
+    type: String,
+    enum: ["Nuevo", "Confirmado", "Preparando", "Enviado", "Entregado", "Cancelado"],
+    default: "Nuevo",
+  },
+  userId: { type: Schema.ObjectId, ref: "user", required: true },
+  products: [
+    {
+      productId: { type: Schema.ObjectId, ref: "product" },
+      productPrice: { type: Number },
+      productName: {type: String},
+      quantity: { type: Number, required: true },
+    },
+  ],
 });
 
 module.exports = mongoose.model("order", orderSchema);
