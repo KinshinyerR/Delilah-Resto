90b276a4df2c9ff5df691363bc10a50e29fcc5b9
KinshinyerR <kinshinyer02@gmail.com>
2021-05-11 15:29:37 -0500

    update model user

--- models/user.model.js
+++ models/user.model.js
@@ -9,7 +9,14 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   phone: { type: Number, required: true },
   address: { type: String, required: true },
-  role: { type: String, required: true },
+  role: {
+    type: String,
+    enum: [
+      "user",
+      "admin"
+    ],
+    default: "user",
+  },
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
